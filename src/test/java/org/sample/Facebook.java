4eae2cb5cf9ccb609c3a7ead005c27baae1cabc9
somasundreswaran <somasundreswaran@gmail.com>
2022-05-22 13:14:47 +0530

    done new work

--- src/test/java/org/sample/Facebook.java
+++ src/test/java/org/sample/Facebook.java
@@ -1,6 +1,8 @@
 package org.sample;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,6 +13,12 @@ public class Facebook {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
+		
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys("somasundreswaran@gmail.com");
+		
+		WebElement pass = driver.findElement(By.id("pass"));
+		pass.sendKeys("soma");
 		
 	}
 
